5ff545996fee373521676ed00f9161edaaeffcec
edgardegantea <edgar.degante.a@gmail.com>
2024-07-03 12:05:53 -0600

    mapaches.info

---
+++ app/Views/edgardegantea.php
@@ -0,0 +1,268 @@
+<!DOCTYPE HTML>
+
+<html>
+<head>
+    <title>edegantea</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/main.css'); ?>"/>
+    <noscript>
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/noscript.css'); ?>"/>
+    </noscript>
+</head>
+<body class="is-preload">
+
+<!-- Wrapper -->
+<div id="wrapper">
+
+    <!-- Header -->
+    <!-- Note: The "styleN" class below should match that of the banner element. -->
+    <header id="header" class="alt style2">
+        <a href="<?php echo base_url('/'); ?>" class="logo"><strong>Mapaches.info</strong> <span></span></a>
+        <nav>
+            <a href="#menu">Menu</a>
+        </nav>
+    </header>
+
+    <!-- Menu -->
+    <nav id="menu">
+        <ul class="links">
+            <li><a href="<?php echo base_url('/'); ?>">Home</a></li>
+            <li><a href="<?php echo base_url('edgardegantea'); ?>">Edgar Degante-Aguilar</a></li>
+        </ul>
+
+    </nav>
+
+    <!-- Banner -->
+    <!-- Note: The "styleN" class below should match that of the header element. -->
+    <section id="banner" class="style6">
+        <div class="inner">
+							<span class="image">
+								<img src="images/pic07.jpg" alt=""/>
+							</span>
+            <header class="major">
+                <h1>edgar degante-aguilar</h1>
+            </header>
+            <div class="content">
+                <p>Desarrollador de software</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- Main -->
+    <div id="main">
+
+        <!-- One -->
+        <section id="one">
+            <div class="inner">
+                <header class="major">
+                    <h2>Resumen biográfico</h2>
+                </header>
+
+
+                <dl>
+                    <dt>Formación:</dt>
+                    <dd>Ingeniero Informático con especialidad en Sistemas de Información</dd>
+                    <dd>Maestro en Sistemas Computacionales con especialidad en Ingeniería de Software y Sistemas
+                        Distribuidos.
+                    </dd>
+
+                </dl>
+
+                <dl>
+                    <dt>Área:</dt>
+                    <dd>Desarrollador Web FullStack con PHP, Java, JavaScript y Python</dd>
+                    <dd>Desarrollador de aplicaciones móviles con Java, Kotlin y Dart</dd>
+                </dl>
+
+                <dl>
+                    <dt>Instructor de cursos:</dt>
+                    <dd>Fundamentos de Programación en C++</dd>
+                    <dd>Laravel Backend y consumo de API Rest con ionic y Flutter</dd>
+                    <dd>Algoritmos y Programación Básica con Java</dd>
+                    <dd>Algoritmos y Programación con Python</dd>
+                    <dd>Desarrollo de Aplicaciones Móviles con Flutter</dd>
+                    <dd>Curso de Certificación de Adobe CS6 y Microsoft Office 2016 de Testing Program</dd>
+                    <dd>Desarrollo Web con Django inicial</dd>
+                    <dd>Desarrollo web con Django intermedio</dd>
+                    <dd>Desarrollo e Implementación de APIs</dd>
+                    <dd>Instructor de cursos del TecNM a nivel nacional.</dd>
+                    <dd>Instructor de Red Hat Academy</dd>
+                </dl>
+
+
+                Desarrollador con tecnologías Open Source
+            </div>
+        </section>
+
+        <!-- Two -->
+
+        <!--
+        <section id="two" class="spotlights">
+            <section>
+                <a href="generic.html" class="image">
+                    <img src="images/pic08.jpg" alt="" data-position="center center"/>
+                </a>
+                <div class="content">
+                    <div class="inner">
+                        <header class="major">
+                            <h3>Orci maecenas</h3>
+                        </header>
+                        <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus
+                            condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna
+                            pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et
+                            sagittis tempus.</p>
+                        <ul class="actions">
+                            <li><a href="generic.html" class="button">Learn more</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <a href="generic.html" class="image">
+                    <img src="images/pic09.jpg" alt="" data-position="top center"/>
+                </a>
+                <div class="content">
+                    <div class="inner">
+                        <header class="major">
+                            <h3>Rhoncus magna</h3>
+                        </header>
+                        <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus
+                            condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna
+                            pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et
+                            sagittis tempus.</p>
+                        <ul class="actions">
+                            <li><a href="generic.html" class="button">Learn more</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <a href="generic.html" class="image">
+                    <img src="images/pic10.jpg" alt="" data-position="25% 25%"/>
+                </a>
+                <div class="content">
+                    <div class="inner">
+                        <header class="major">
+                            <h3>Sed nunc ligula</h3>
+                        </header>
+                        <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus
+                            condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna
+                            pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et
+                            sagittis tempus.</p>
+                        <ul class="actions">
+                            <li><a href="generic.html" class="button">Learn more</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </section>
+        -->
+
+        <!-- Three -->
+        <!--
+        <section id="three">
+            <div class="inner">
+                <header class="major">
+                    <h2>Massa libero</h2>
+                </header>
+                <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna
+                    sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas
+                    laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat
+                    tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et
+                    feugiat tempus.</p>
+                <ul class="actions">
+                    <li><a href="generic.html" class="button next">Get Started</a></li>
+                </ul>
+            </div>
+        </section>
+        -->
+
+    </div>
+
+    <!-- Contact -->
+    <!--
+    <section id="contact">
+        <div class="inner">
+            <section>
+                <form method="post" action="#">
+                    <div class="fields">
+                        <div class="field half">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" id="name"/>
+                        </div>
+                        <div class="field half">
+                            <label for="email">Email</label>
+                            <input type="text" name="email" id="email"/>
+                        </div>
+                        <div class="field">
+                            <label for="message">Message</label>
+                            <textarea name="message" id="message" rows="6"></textarea>
+                        </div>
+                    </div>
+                    <ul class="actions">
+                        <li><input type="submit" value="Send Message" class="primary"/></li>
+                        <li><input type="reset" value="Clear"/></li>
+                    </ul>
+                </form>
+            </section>
+            <section class="split">
+                <section>
+                    <div class="contact-method">
+                        <span class="icon solid alt fa-envelope"></span>
+                        <h3>Email</h3>
+                        <a href="#">information@untitled.tld</a>
+                    </div>
+                </section>
+                <section>
+                    <div class="contact-method">
+                        <span class="icon solid alt fa-phone"></span>
+                        <h3>Phone</h3>
+                        <span>(000) 000-0000 x12387</span>
+                    </div>
+                </section>
+                <section>
+                    <div class="contact-method">
+                        <span class="icon solid alt fa-home"></span>
+                        <h3>Address</h3>
+                        <span>1234 Somewhere Road #5432<br/>
+										Nashville, TN 00000<br/>
+										United States of America</span>
+                    </div>
+                </section>
+            </section>
+        </div>
+    </section>
+    -->
+
+    <!-- Footer -->
+    <footer id="footer">
+        <div class="inner">
+            <ul class="icons">
+                <li><a href="https://www.youtube.com/@edgardegantea" class="icon brands alt fa-youtube" target="_blank"><span class="label">Youtube</span></a></li>
+                <li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
+                <li><a href="https://www.facebook.com/profile.php?id=100091902671608" class="icon brands alt fa-facebook-f" target="_blank"><span class="label">Facebook</span></a></li>
+                <li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+                <li><a href="https://github.com/edgardegantea" class="icon brands alt fa-github" target="_blank"><span class="label">GitHub</span></a></li>
+                <li><a href="https://www.linkedin.com/in/edgar-degante-aguilar-592296124/" class="icon brands alt fa-linkedin-in" target="_blank"><span class="label">LinkedIn</span></a></li>
+            </ul>
+            <ul class="copyright">
+                <li>&copy; <?php echo date('Y'); ?></li>
+                <li>Developed by: <a href="#">edegantea</a></li>
+            </ul>
+        </div>
+    </footer>
+
+</div>
+
+<!-- Scripts -->
+<script src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/jquery.scrolly.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/jquery.scrollex.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/browser.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/breakpoints.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/util.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/main.js'); ?>"></script>
+
+</body>
+</html>
---
+++ app/Views/welcome_message.php
@@ -0,0 +1,219 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>mapaches.info</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/main.css'); ?>"/>
+    <noscript>
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/noscript.css'); ?>"/>
+    </noscript>
+</head>
+<body class="is-preload">
+
+<!-- Wrapper -->
+<div id="wrapper">
+
+    <!-- Header -->
+    <header id="header" class="alt">
+        <a href="<?php echo base_url('/'); ?>" class="logo"><strong>Mapaches.info</strong> <span></span></a>
+        <nav>
+            <a href="#menu">Menu</a>
+        </nav>
+    </header>
+
+    <!-- Menu -->
+    <nav id="menu">
+        <ul class="links">
+            <li><a href="<?php echo base_url('/'); ?>">Home</a></li>
+            <li><a href="<?php echo base_url('edgardegantea'); ?>">Edgar Degante-Aguilar</a></li>
+        </ul>
+
+        <!--
+        <ul class="actions stacked">
+            <li><a href="#" class="button primary fit">Get Started</a></li>
+            <li><a href="#" class="button fit">Log In</a></li>
+        </ul>
+        -->
+    </nav>
+
+    <!-- Banner -->
+    <section id="banner" class="major"
+             style="height: 100%; background-image: url(<?php echo base_url('images/fondo01.jpg') ?>);">
+        <div class="inner">
+            <header class="major">
+                <h1>mapaches.info</h1>
+            </header>
+            <div class="content">
+                <p></p>
+                <!--
+                <ul class="actions">
+                    <li><a href="#one" class="button next scrolly">Get Started</a></li>
+                </ul>
+                -->
+            </div>
+        </div>
+    </section>
+
+    <!-- Main -->
+    <div id="main">
+
+        <!-- One -->
+        <!--<section id="one" class="tiles">
+            <article>
+									<span class="image">
+										<img src="images/pic01.jpg" alt=""/>
+									</span>
+                <header class="major">
+                    <h3><a href="landing.html" class="link">Aliquam</a></h3>
+                    <p>Ipsum dolor sit amet</p>
+                </header>
+            </article>
+            <article>
+									<span class="image">
+										<img src="images/pic02.jpg" alt=""/>
+									</span>
+                <header class="major">
+                    <h3><a href="landing.html" class="link">Tempus</a></h3>
+                    <p>feugiat amet tempus</p>
+                </header>
+            </article>
+            <article>
+									<span class="image">
+										<img src="images/pic03.jpg" alt=""/>
+									</span>
+                <header class="major">
+                    <h3><a href="landing.html" class="link">Magna</a></h3>
+                    <p>Lorem etiam nullam</p>
+                </header>
+            </article>
+            <article>
+									<span class="image">
+										<img src="images/pic04.jpg" alt=""/>
+									</span>
+                <header class="major">
+                    <h3><a href="landing.html" class="link">Ipsum</a></h3>
+                    <p>Nisl sed aliquam</p>
+                </header>
+            </article>
+            <article>
+									<span class="image">
+										<img src="images/pic05.jpg" alt=""/>
+									</span>
+                <header class="major">
+                    <h3><a href="landing.html" class="link">Consequat</a></h3>
+                    <p>Ipsum dolor sit amet</p>
+                </header>
+            </article>
+            <article>
+									<span class="image">
+										<img src="images/pic06.jpg" alt=""/>
+									</span>
+                <header class="major">
+                    <h3><a href="landing.html" class="link">Etiam</a></h3>
+                    <p>Feugiat amet tempus</p>
+                </header>
+            </article>
+        </section>-->
+
+        <!-- Two -->
+        <!--<section id="two">
+            <div class="inner">
+                <header class="major">
+                    <h2>Massa libero</h2>
+                </header>
+                <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna
+                    sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas
+                    laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat
+                    tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et
+                    feugiat tempus.</p>
+                <ul class="actions">
+                    <li><a href="landing.html" class="button next">Get Started</a></li>
+                </ul>
+            </div>
+        </section>-->
+
+    </div>
+
+    <!-- Contact -->
+    <section id="contact">
+        <div class="inner">
+            <section>
+                <form method="post" action="#">
+                    <div class="fields">
+                        <div class="field half">
+                            <label for="name">Nombre</label>
+                            <input type="text" name="name" id="name"/>
+                        </div>
+                        <div class="field half">
+                            <label for="email">Email</label>
+                            <input type="text" name="email" id="email"/>
+                        </div>
+                        <div class="field">
+                            <label for="message">Mensaje</label>
+                            <textarea name="message" id="message" rows="6"></textarea>
+                        </div>
+                    </div>
+                    <ul class="actions">
+                        <li><input type="submit" value="Enviar mensaje" class="primary"/></li>
+                        <li><input type="reset" value="Limpiar"/></li>
+                    </ul>
+                </form>
+            </section>
+            <section class="split">
+                <section>
+                    <div class="contact-method">
+                        <span class="icon solid alt fa-envelope"></span>
+                        <h3>Email</h3>
+                        <a href="#">edgar.degante.a@gmail.com</a>
+                    </div>
+                </section>
+                <section>
+                    <div class="contact-method">
+                        <span class="icon solid alt fa-phone"></span>
+                        <h3>Teléfono</h3>
+                        <span>(+52) 231 205 1120</span>
+                    </div>
+                </section>
+                <section>
+                    <div class="contact-method">
+                        <span class="icon solid alt fa-home"></span>
+                        <h3>Dirección</h3>
+                        <span>Teziutlán, Puebla, México</span>
+                    </div>
+                </section>
+            </section>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer id="footer">
+        <div class="inner">
+            <ul class="icons">
+                <li><a href="https://www.youtube.com/@edgardegantea" class="icon brands alt fa-youtube" target="_blank"><span class="label">Youtube</span></a></li>
+                <li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
+                <li><a href="https://www.facebook.com/profile.php?id=100091902671608" class="icon brands alt fa-facebook-f" target="_blank"><span class="label">Facebook</span></a></li>
+                <li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+                <li><a href="https://github.com/edgardegantea" class="icon brands alt fa-github" target="_blank"><span class="label">GitHub</span></a></li>
+                <li><a href="https://www.linkedin.com/in/edgar-degante-aguilar-592296124/" class="icon brands alt fa-linkedin-in" target="_blank"><span class="label">LinkedIn</span></a></li>
+            </ul>
+            <ul class="copyright">
+                <li>&copy; <?php echo date('Y'); ?></li>
+                <li>Developed by: <a href="#">edegantea</a></li>
+            </ul>
+        </div>
+    </footer>
+
+</div>
+
+<!-- Scripts -->
+<script src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/jquery.scrolly.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/jquery.scrollex.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/browser.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/breakpoints.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/util.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/main.js'); ?>"></script>
+
+</body>
+</html>
